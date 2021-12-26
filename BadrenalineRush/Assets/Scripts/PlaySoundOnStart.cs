b7b2498701cf1240585f1371bd688dca20fc977f
drinkingWater <aamuti64@gmail.com>
2021-12-26 14:44:11 +0600

    adding sound

---
+++ BadrenalineRush/Assets/Scripts/PlaySoundOnStart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySoundOnStart : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip[]
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+}

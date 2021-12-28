a12c4e9deeaff28e3c15885dcbd5fc86abdfe01b
drinkingWater <aamuti64@gmail.com>
2021-12-28 18:46:41 +0600

    added pause menu

---
+++ BadrenalineRush/Assets/LevelManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField]
+    private bool isPause;
+    [SerializeField]
+    private Canvas canvas;
+
+    private void goPause()
+    {
+        isPause = !isPause;
+        canvas.enabled = isPause;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPause = canvas.enabled;
+        canvas.enabled = false;
+        isPause = canvas.enabled;
+    }
+
+    public void changeScene(string scene)
+    {
+        GameManeger.instance.sceneChanger(scene);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            goPause();
+        }
+    }
+}
